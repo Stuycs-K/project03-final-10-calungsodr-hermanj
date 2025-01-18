0c66729f162537db7942c3de61609600b1b962fd
Joy Herman <jherman50@stuy.edu>
2025-01-18 13:34:40 -0500

    fixed the tie and added a warning to the instructions

--- host.c
+++ host.c
@@ -49,7 +49,7 @@ void print_points(){
 		winner_num = i;
 		tie = 1;
 	  }
-	  if (players[i].score == winner_points) {
+	 else if (players[i].score == winner_points) {
 		tie = 0;
 	  }
 	// possible add-on -- tell who the winner is
@@ -143,7 +143,7 @@ int main(){
 
     close(from_client);
 
-    printf("\nWelcome to the game! After choosing a topic, each player will take turns to\nanswer a series of questions. One correct answer = one point for your score. If you wish to exit the game at any point, type 'end' instead\nof your answer.\n\n");
+    printf("\nWelcome to the game! After choosing a topic, each player will take turns to\nanswer a series of questions. One correct answer = one point for your score. If you wish to exit the game at any point, type 'end' instead\nof your answer.\nIf you dare answer when it is NOT your turn, you shall be punished and will not\nbe able to answer any more questions!!\n\n");
     printf("Please choose a topic (History, Geography, Math, Science): ");
 
     char topic[20];
